5cfc0049b9ad3cfb9083d7390172ceac5c35bec7
shubham-2143 <shubhamsontakke0062@gmail.com>
2026-03-18 18:36:08 -0400

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://280428843735.realhandsonlabs.net "
+const API_BASE_URL = "http://shubhamonline.shop"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
