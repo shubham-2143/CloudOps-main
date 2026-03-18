42663d4ab831d5b28330c14a491c8cff81fcb0d3
shubham-2143 <shubhamsontakke0062@gmail.com>
2026-03-18 17:55:11 -0400

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "http://280428843735.realhandsonlabs.net"
+const API_BASE_URL = "http://a8282314d9aac44eaba19fc6ca5835d9-2115724880.us-east-1.elb.amazonaws.com"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
